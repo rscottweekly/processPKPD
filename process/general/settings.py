a68b6b20cac1d5815c8a655707a493c27baf6211
Ross Scott-Weekly <rscottweekly@gmail.com>
2014-10-03 13:33:07 +1300

    Added constants

--- process/general/settings.py
+++ process/general/settings.py
@@ -5,3 +5,8 @@ import os
 
 coding_file = os.path.join(process_settings.base_dir, "CodingInformation.csv")
 
+const_R = 8.314462175 #L kPa K-1 mol -1
+const_T37 =  310.15 #K
+const_PH2O = 6.275 #kPa
+const_PH2OmmHg = 47 #mmHg for alveolar gas eqn
+
