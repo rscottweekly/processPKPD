7b4cef32a284740d134813c816406f15a43a27e0
Ross Scott-Weekly <rscottweekly@gmail.com>
2014-10-03 13:27:25 +1300

    More monitorying
    Directories set

--- process/MonitorData/settings.py
+++ process/MonitorData/settings.py
@@ -2,3 +2,7 @@ __author__ = 'rscottweekly'
 
 from process import settings
 import os
+
+dir_monitor_raw = os.path.join(settings.base_dir, "Monitor Data/Raw")
+prod_dir_monitor = os.path.join(settings.base_dir, "Monitor Data/1Min")
+
